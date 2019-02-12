e5543f71e438bf3759c9e94dba0d8068b92d3c0e
anton <ixxvivxxi@yahoo.com>
2019-02-13 01:21:24 +0300

    fix zoom on links

--- frontend/app/helpers/zoom-for-segment.js
+++ frontend/app/helpers/zoom-for-segment.js
@@ -1,11 +1,29 @@
 import { helper } from '@ember/component/helper';
 
-export function zoomForSegment([roadType]) {
-  const roadTypes = [2, 3, 4, 5, 7];
-  if (roadTypes.includes(+roadType)) {
+export function zoomForSegment([length]) {
+
+  if (+length > 5000) {
+    return 1;
+  }
+
+  if (+length > 3000) {
+    return 2;
+  }
+
+  if (+length > 1000) {
     return 3;
   }
-  return 5;
+
+  if (+length > 300) {
+    return 4;
+  }
+
+  if (+length > 150) {
+    return 5;
+  }
+  
+  return 6;
+  
 }
 
 export default helper(zoomForSegment);
