ecfab13eb3090c2c9f5c99cf0bcedfb8580485ef
ixxvivxxi <ixxvivxxi@yahoo.com>
2019-04-13 15:04:09 +0300

    fix Major Highway and Walking Trail ids

--- backend-php/application/migrations/008_create_road_type_table.php
+++ backend-php/application/migrations/008_create_road_type_table.php
@@ -12,16 +12,16 @@ class Migration_Create_road_type_table extends CI_Migration
     public function up()
     {
         $fields = [
-            'id'   => [
-                'type'           => 'INT',
-                'unsigned'       => true,
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
                 'auto_increment' => true,
             ],
             'name' => [
-                'type'       => 'VARCHAR',
+                'type' => 'VARCHAR',
                 'constraint' => 50,
-                'null'       => false,
-                'default'    => '',
+                'null' => false,
+                'default' => '',
             ],
         ];
         $this->dbforge->add_field($fields);
@@ -30,73 +30,72 @@ class Migration_Create_road_type_table extends CI_Migration
 
         $roadTypes = [
             [
-                'id'   => 1,
+                'id' => 1,
                 'name' => 'Street',
             ],
             [
-                'id'   => 2,
+                'id' => 2,
                 'name' => 'Primary Street',
             ],
             [
-                'id'   => 3,
+                'id' => 3,
                 'name' => 'Freeway',
             ],
             [
-                'id'   => 4,
+                'id' => 4,
                 'name' => 'Ramp',
             ],
             [
-                'id'   => 5,
-                'name' => 'Major Highway',
-            ],
-            [
-                'id'   => 6,
+                'id' => 5,
                 'name' => 'Walking Trail',
             ],
             [
-                'id'   => 7,
+                'id' => 6,
+                'name' => 'Major Highway',
+            ],
+            [
+                'id' => 7,
                 'name' => 'Minor Highway',
             ],
             [
-                'id'   => 8,
+                'id' => 8,
                 'name' => 'Off-road / Not maintained',
             ],
             [
-                'id'   => 10,
+                'id' => 10,
                 'name' => 'Pedestrian Boardwalk',
             ],
             [
-                'id'   => 15,
+                'id' => 15,
                 'name' => 'Ferry',
             ],
             [
-                'id'   => 16,
+                'id' => 16,
                 'name' => 'Stairway',
             ],
             [
-                'id'   => 17,
+                'id' => 17,
                 'name' => 'Private Road',
             ],
             [
-                'id'   => 18,
+                'id' => 18,
                 'name' => 'Railroad',
             ],
             [
-                'id'   => 19,
+                'id' => 19,
                 'name' => 'Runway/Taxiway',
             ],
             [
-                'id'   => 20,
+                'id' => 20,
                 'name' => 'Parking Lot Road',
             ],
             [
-                'id'   => 22,
+                'id' => 22,
                 'name' => 'Narrow Street',
             ],
         ];
 
         $this->db->insert_batch('road_type', $roadTypes);
-
         echo '<li>up 008 Create_road_type_table</li>';
     }
 
